bcf8f6c5579c65e30eff376b35414588c023bc9c
huyth <huyth2403@gmail.com>
2022-02-19 22:41:38 +0700

    bieu do theo tuan

--- src/layouts/bieu-do-theo-tuan/index.js
+++ src/layouts/bieu-do-theo-tuan/index.js
@@ -1,23 +1,7 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // @mui material components
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import ReactECharts from 'echarts-for-react'
-
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -27,162 +11,237 @@ import MDTypography from "components/MDTypography";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
+import { useEffect, useState } from "react";
+import { instance } from "../../service/service";
 
-const nhietDo = {
-  title: {
-    show: false,
-    text: 'Nhiệt độ'
-  },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross',
-      crossStyle: {
-        color: '#999'
-      }
-    }
-  },
-  legend: {
-    data: ['Max', 'Min', 'Avg']
-  },
-  xAxis: [
-    {
-      type: 'category',
-      data: ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'CN'],
-      axisPointer: {
-        type: 'shadow'
-      }
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value',
-      name: '°C',
-      min: 0,
-      max: 250,
-      interval: 50,
-      axisLabel: {
-        formatter: '{value} °C'
-      }
-    },
-    {
-      type: 'value',
-      name: '',
-      min: 0,
-      max: 25,
-      interval: 5,
-      axisLabel: {
-        formatter: ''
-      }
-    }
-  ],
-  series: [
-    {
-      name: 'Max',
-      type: 'bar',
-      data: [
-        2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
-      ]
-    },
-    {
-      name: 'Min',
-      type: 'bar',
-      data: [
-        2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
-      ]
-    },
-    {
-      name: 'Avg',
-      type: 'line',
-      yAxisIndex: 1,
-      data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-    }
-  ]
-};
-const doAm = {
-  title: {
-    show: false,
-    text: 'Độ ẩm'
-  },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross',
-      crossStyle: {
-        color: '#999'
-      }
-    }
-  },
-  toolbox: {
-    show: false,
-    feature: {
-      dataView: { show: true, readOnly: false },
-      magicType: { show: true, type: ['line', 'bar'] },
-      restore: { show: true },
-      saveAsImage: { show: false }
-    }
-  },
-  legend: {
-    data: ['Max', 'Min', 'Avg']
-  },
-  xAxis: [
-    {
-      type: 'category',
-      data: ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'CN'],
-      axisPointer: {
-        type: 'shadow'
-      }
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value',
-      name: '%',
-      min: 0,
-      max: 250,
-      interval: 50,
-      axisLabel: {
-        formatter: '{value} %'
-      }
-    },
-    {
-      type: 'value',
-      name: '',
-      min: 0,
-      max: 25,
-      interval: 5,
-      axisLabel: {
-        formatter: ''
-      }
-    }
-  ],
-  series: [
-    {
-      name: 'Max',
-      type: 'bar',
-      data: [
-        2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
-      ]
-    },
-    {
-      name: 'Min',
-      type: 'bar',
-      data: [
-        2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
-      ]
-    },
-    {
-      name: 'Avg',
-      type: 'line',
-      yAxisIndex: 1,
-      data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-    }
-  ]
-};
 
+let interval = null
 function Tables() {
-  return (
-    <DashboardLayout>
+
+  const [dataNhietDo, setDataNhietDo] = useState([])
+
+  const [dataDoAm, setDataDoAm] = useState([])
+
+  const nhietDo = {
+    title: {
+      show: false,
+      text: 'Nhiệt độ'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        crossStyle: {
+          color: '#999'
+        }
+      }
+    },
+    legend: {
+      data: ['Max', 'Min', 'Avg']
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'CN'],
+        axisPointer: {
+          type: 'shadow'
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        name: '°C',
+        min: 0,
+        max: 100,
+        interval: 50,
+        axisLabel: {
+          formatter: '{value} °C'
+        }
+      },
+      {
+        type: 'value',
+        name: '',
+        min: 0,
+        max: 100,
+        interval: 5,
+        axisLabel: {
+          formatter: ''
+        }
+      }
+    ],
+    series: [
+      {
+        name: 'Max',
+        type: 'bar',
+        data: dataNhietDo.map(el => el.max)
+      },
+      {
+        name: 'Min',
+        type: 'bar',
+        data: dataNhietDo.map(el => el.min)
+      },
+      {
+        name: 'Avg',
+        type: 'line',
+        yAxisIndex: 1,
+        data: dataNhietDo.map(el => el.avg)
+      }
+    ]
+  }
+
+  const doAm = {
+    title: {
+      show: false,
+      text: 'Độ ẩm'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        crossStyle: {
+          color: '#999'
+        }
+      }
+    },
+    toolbox: {
+      show: false,
+      feature: {
+        dataView: { show: true, readOnly: false },
+        magicType: { show: true, type: ['line', 'bar'] },
+        restore: { show: true },
+        saveAsImage: { show: false }
+      }
+    },
+    legend: {
+      data: ['Max', 'Min', 'Avg']
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'CN'],
+        axisPointer: {
+          type: 'shadow'
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        name: '%',
+        min: 0,
+        max: 100,
+        interval: 50,
+        axisLabel: {
+          formatter: '{value} %'
+        }
+      },
+      {
+        type: 'value',
+        name: '',
+        min: 0,
+        max: 100,
+        interval: 5,
+        axisLabel: {
+          formatter: ''
+        }
+      }
+    ],
+    series: [
+      {
+        name: 'Max',
+        type: 'bar',
+        data: dataDoAm.map(el => el.max)
+      },
+      {
+        name: 'Min',
+        type: 'bar',
+        data: dataDoAm.map(el => el.min)
+      },
+      {
+        name: 'Avg',
+        type: 'line',
+        yAxisIndex: 1,
+        data: dataDoAm.map(el => el.avg)
+      }
+    ]
+  }
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      instance({
+        method: 'get',
+        url: '/alarm/get-all-heart-beat-current-week?token=Y3ODkwIiwi'
+      }).then(resp => {
+        const dataChart = [[], [], [], [], [], [], []]
+        if (resp?.data?.data?.length > 0) {
+          let flag = resp?.data?.data[0].createdDate.split('T')[0]
+          let i = 0
+          resp.data.data.forEach(el => {
+            if (flag !== el.createdDate.split('T')[0]) {
+              dataChart[i+1].push(el)
+              flag = el.createdDate.split('T')[0]
+              i++
+            } else {
+              dataChart[i].push(el)
+            }
+          })
+          const dataView = []
+          const dataViewTemp = []
+
+          for(let j = 0; j < dataChart.length; j++) {
+            if (dataChart[j].length > 0) {
+              let min = dataChart[j][0].humidity, max = 0;
+              let min1 = dataChart[j][0].temperature, max1 = 0;
+              dataChart[j].forEach(el => {
+                // do am
+                if (min > el.humidity) {
+                  min = el.humidity
+                }
+                if (max < el.humidity) {
+                  max = el.humidity
+                }
+
+                // nhiet do
+                if (min1 > el.temperature) {
+                  min1 = el.temperature
+                }
+                if (max1 < el.temperature) {
+                  max1 = el.temperature
+                }
+              })
+              dataView.push({
+                min,
+                max,
+                avg: (min + max) / 2
+              })
+              dataViewTemp.push({
+                min: min1,
+                max: max1,
+                avg: (min1 + max1) / 2
+              })
+            } else {
+              dataView.push({
+                min: 0,
+                max: 0,
+                avg: 0
+              })
+              dataViewTemp.push({
+                min: 0,
+                max: 0,
+                avg: 0
+              })
+            }
+          }
+          setDataDoAm(dataView)
+          setDataNhietDo(dataViewTemp)
+        }
+      });
+    }, 10000)
+    return () => clearInterval(interval)
+  }, [])
+
+  return <DashboardLayout>
       <DashboardNavbar />
       <MDBox pb={5}>
         <Grid container spacing={6}>
@@ -246,7 +305,6 @@ function Tables() {
       </MDBox>
       <Footer />
     </DashboardLayout>
-  );
 }
 
 export default Tables;
